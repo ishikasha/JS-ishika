16406dec7a6892c34f72b8bca215e3e828ba72a1
Ishika <88087713+ishikasha@users.noreply.github.com>
2025-04-19 13:54:13 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountID =1444432
+let accountEmail= "ishika@google.com"
+var accountPassword ="123456"
+accountCity="jaipur"
+let accountState; //it will consider as undefined.
+// console.log(accountID= 2); not allowed
+//  prefer not to use var as it is not blocked and function define
+
+accountEmail="is@gmail.com"
+accountPassword=" 121212121"
+accountCity="Gurugurm"
+
+console.table([accountEmail, accountPassword, accountCity ])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"// use all JS code as new version;
+// alert(3+3) it will not run as it is not browser it is node js.
+// make sure code should be readble 
+
+//datatypes-
+// number
+// string
+// boolean
+//null standalone value
+// undefine its type will be undefine.
+//symbol unique
+
+console.log (typeof(null))// object
+console.log( typeof(undefined))// undefine
