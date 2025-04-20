4a4224f08a44e212d6d2815cb0af00680b96f36c
Ishika <88087713+ishikasha@users.noreply.github.com>
2025-04-20 10:58:02 +0000

    conversion and operations

---
+++ 01_basics/03_datatypo_conversion.js
@@ -0,0 +1,35 @@
+
+let score= "33"
+// console.log(typeof(score));
+
+let valueIsNumber = Number(score)
+// console.log(typeof(valueIsNumber));
+// console.log(valueIsNumber)
+
+// conversion types 
+// "33abc" is not string but it will convert it will not convert to numer it will print NaN.
+// "ishika " will not convert to number output will be NaN(not a number)
+// if I print 1 ==> true conersion , 0==> false
+
+let isBoolean = 1
+let numberIsBoolean =Boolean(isBoolean)
+//console.log(numberIsBoolean)
+
+//1=> true, 0=>  false
+//"" it will show false
+// "ishika" it will show true
+
+let someNumber =33
+let numberIsString = String(someNumber)
+// console.log(numberIsString);
+// console.log(typeof(numberIsString));
+
+//*******************************  OPERATIONS  ******************************************** */
+
+let str1= "ishika"
+let str2 = " bhardwaj"
+let str3= (str1+str2);
+console.log(str3);
+
+
+
